4d68dc080161edf8632a13a14a4da1c49d4c9c87
peterx <peterx@iastate.edu>
2018-03-14 17:53:17 -0500

    add vue

---
+++ iview-project/src/components/signup.vue
@@ -0,0 +1,42 @@
+<template>
+	<form method="POST" action="http://localhost:8080/register"> 		
+	<div id="content">
+
+		<Input id="name" v-model="value2" type="text" placeholder="Enter your nickname" style="width: 300px">
+        <Icon slot="prepend"  type="person" style="width:20px" />
+        </Input>
+        <br>
+        <Input id="email" v-model="value1" type="email" placeholder="Enter your email" style="width: 300px">        
+        <Icon slot="prepend"  type="email" style="width:20px" />
+        </Input>
+        <br>
+        <Input id="password" v-model="value3" type="password" placeholder="Enter your password" style="width: 300px">
+        <Icon slot="prepend"  type="locked" style="width:20px"/>
+        </Input>
+        <br>
+        <Input id="password-confirm" v-model="value3" type="password" placeholder="Confirm your password" style="width: 300px">
+        <Icon slot="prepend"  type="locked" style="width:20px"/>
+        </Input>
+        <br>
+        <center>
+       		<Button type="submit" shape="circle">Submit</Button>
+        </center>
+
+	</div>
+	</form>
+</template>
+
+<script type="text/javascript">
+	
+
+
+</script>
+
+<style>
+#content{
+	position:absolute;
+	top: 30%;
+	right: 10%;
+
+}
+</style>
---
+++ iview-project/src/router/index.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import temp from '@/components/temp'
+import signup from '@/components/signup'
+import login from '@/components/login'
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/temp',
+      name: 'temp',
+      component: temp
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: signup
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: login
+    },
+  ]
+})
